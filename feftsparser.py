a415644c5e54555e44f1802740af4928ab6b298a
PayneGray <rlacsamana@csu.fullerton.edu>
2018-01-06 19:41:30 -0800

    putting characters in dictionaries for later use

---
+++ feftsparser.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf_8 -*-
+
+emote = {
+"sweat":"汗" ,
+"blush":"照",
+"default":"通常",
+"surprise":"びっくり",
+"angry":"怒",
+"hurt":"苦",
+"laugh":"笑",
+"smug":"キメ",
+"yell":"やけくそ",
+"possessed":"囚"}
+neutral1 = {
+"Corrin":"MU",
+"Anna":"アンナ",
+"Azura":"アクア",
+"Felicia":"フェリシア",
+"Izana":"イザナ",
+"Jakob":"ジョーカー",
+"Kaze":"モズメ",
+"Shura":"アシュラ",
+"Silas":"サイラス"}
+
+neutral2 = {
+"Dwyer":"ディーア", 
+"KanaF":"カンナ男", 
+"KanaM":"カンナ女", 
+"Midori":"ミドリコ", 
+"Shigure":"シグレ", 
+"Sophie":"ゾフィー"}
+
+
